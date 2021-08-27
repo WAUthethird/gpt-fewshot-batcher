efb4e48457ff6d05f85002f4ecb71a7e814c4185
WAUthethird <unsworthwa@outlook.com>
2021-08-27 12:50:12 -0600

    Added menu bar

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import PySimpleGUI as sg
 
 sg.theme('Dark Blue 3')
 
+menu_def = [['&File', ['COULD DO MORE STUFF HERE', '&Options', 'E&xit']],
+            ['&Help', ['&About']]]
+
 data = [["INPUT", "OUTPUT", "TOKENCOUNT"], ["INPUT2", "OUTPUT2", "TOKENCOUNT2"], ["INPUT3", "OUTPUT3", "TOKENCOUNT3"], ["INPUT4", "OUTPUT4", "TOKENCOUNT4"], ["INPUT5", "OUTPUT5", "TOKENCOUNT5"], ["INPUT6", "OUTPUT6", "TOKENCOUNT6"], ["INPUT7", "OUTPUT7", "TOKENCOUNT7"], ["INPUT8", "OUTPUT8", "TOKENCOUNT8"], ["Line1\nLine2\nLine3", "Line1\nLine2\nLine3", "Line1\nLine2\nLine3"]]
 headings = ["Input", "Output", "Token Count"]
 
 side_buttons_table = [[sg.Text('Fewshot List Options')],
                       [sg.Button('Display selected pair')], # This should generate a popup asking if they're sure - it'll wipe whatever they've already got in there. We don't need to show the popup if they don't have anything in the boxes, though.
-                      [sg.Button('Remove selected pair')],
+                      [sg.Button('Remove selected pair(s)')],
                       [sg.Button('Save fewshots to file')],
                       [sg.Button('Load fewshots from file')],
                       [sg.Button('Clear fewshot table')]]
@@ -18,7 +21,8 @@ side_buttons_input = [[sg.Text('Current Pair Options')],
                       [sg.Button('Preview trimmed pairs')],
                       [sg.Button('Clear input and output')]]
 
-main_layout = [[sg.Button('Change input prefix'), sg.Button('Change output prefix'), sg.Button('Export')],
+main_layout = [[sg.Menu(menu_def)],
+               [sg.Button('Change input prefix'), sg.Button('Change output prefix'), sg.Button('Export')],
                [sg.Table(values=data, headings=headings, max_col_width=100,
                                 background_color='darkblue',
                                 auto_size_columns=True,
